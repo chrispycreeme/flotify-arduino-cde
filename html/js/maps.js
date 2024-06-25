feac0c55b6fad0df4b003614ca5c73ec5b39f5bf
CDave2 <202210819@gordoncollege.edu.ph>
2024-06-25 22:53:57 +0800

    june 25, 2024

--- html/js/maps.js
+++ html/js/maps.js
@@ -5,20 +5,44 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var customIcon = L.icon({
-    iconUrl: 'img/location_on_48dp_FILL1_wght400_GRAD0_opsz48.png',
-    iconSize:     [60, 60],
-    iconAnchor:   [22, 94],
-    popupAnchor:  [-3, -76]
+var legend = L.control({ position: 'bottomright' });
+
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'legend');
+
+    div.innerHTML += '<h4>Legend</h4>';
+    div.innerHTML += '<i style="background: url(assets/icons/location_on_48dp_FILL1_wght400_GRAD0_opsz48.png) no-repeat center; width: 10px; height: 10px; display: inline-block;"></i> Station Location<br>';
+    div.innerHTML += '<i style="background: url(assets/icons/shield_with_house_48dp_FILL1_wght300_GRAD200_opsz20.png) no-repeat center; width: 10px; height: 10px; display: inline-block;"></i> Evacuation Location<br>';
+
+    return div;
+};
+
+legend.addTo(map);
+
+var stationLocation = L.icon({
+    iconUrl: 'assets/icons/location_on_48dp_FILL1_wght400_GRAD0_opsz48.png',
+    iconSize: [40, 40],
 });
 
-var marker = L.marker([14.836375, 120.278112], {icon: customIcon}).addTo(map);
-var marker2 = L.marker([9.776641697816485, 118.76523128023013], {icon: customIcon}).addTo(map);
+var marker = L.marker([14.836375, 120.278112], { icon: stationLocation }).addTo(map);
+var marker2 = L.marker([9.776641697816485, 118.76523128023013], { icon: stationLocation }).addTo(map);
 
-marker.on('click', function(e) {
+marker.on('click', function (e) {
     map.flyTo(e.target.getLatLng(), 18);
 });
 
-marker2.on('click', function(e) {
+marker2.on('click', function (e) {
     map.flyTo(e.target.getLatLng(), 18);
 });
+
+var evacuationLocation = L.icon({
+    iconUrl: 'assets/icons/shield_with_house_48dp_FILL1_wght300_GRAD200_opsz20.png',
+    iconSize: [40, 40],
+});
+
+var evacuation1 = L.marker([14.849261885135427, 120.31312129680448], { icon: evacuationLocation }).addTo(map);
+
+evacuation1.on('click', function (e) {
+    map.flyTo(e.target.getLatLng(), 18);
+});
+
